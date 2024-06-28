7c8d3e8f7d5da54712421dadad1d02cdf79aa8a1
Piter <vbaApp@outlook.com>
2024-06-28 20:52:28 +0200

    change path to mount

--- Lab5/app.py
+++ Lab5/app.py
@@ -8,7 +8,7 @@ startTime = datetime.now()
 import os
 st.write("Current working directory:", os.getcwd())
 
-filename = "model.h5"
+filename = "/mount/src/sum/model.h5"
 model = pickle.load(open(filename, "rb"))
 
 sex_d = {0: "Kobieta", 1:"Mężczyzna"}
